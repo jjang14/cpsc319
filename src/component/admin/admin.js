db0e00eef0fb1c950bac0fed20734e7f9aea74cd
jungwookjang <b8n2b@ugrad.cs.ubc.ca>
2020-03-09 22:34:31 -0700

    hello

--- src/component/admin/admin.js
+++ src/component/admin/admin.js
@@ -1,6 +1,46 @@
-import React from "react";
-const Admin = () => {
-  return <div>hi</div>;
-};
+import React from 'react';
+import UserTable from '../subComponents/UserTable';
+import LocationTable from '../subComponents/LocationTable';
+import SkillTable from '../subComponents/SkillTable';
+import DisciplineTable from '../subComponents/DisciplineTable';
+import '../../css/admin.css';
+import Sidebar from "../subComponents/Sidebar";
 
-export default Admin;
+export default class Admin extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      current_table:"user"
+    };
+  }
+  setTable = (table) => {
+    this.setState({current_table:table})
+  }
+
+  render() {
+
+    let table = null;
+    if(this.state.current_table == "user") {
+      table = <div className = "dataTable"> <UserTable/> </div>;
+    }else if(this.state.current_table == "location") {
+      table = <div className = "dataTable"> <LocationTable/> </div>
+    } else if(this.state.current_table == "skill") {
+      table = <div className = "dataTable"> <SkillTable/> </div>
+    } else {
+      table = <div className = "dataTable"> <DisciplineTable/> </div>
+    }
+
+    return (
+        <div className="adminMain">
+          <Sidebar table={this.setTable}/>
+          <div className= "dataTable">
+            {table}
+          </div>
+        </div>
+    );
+  }
+
+}
+
+
